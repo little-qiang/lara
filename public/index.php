3eacf07e06a8897ffaab258f3e9acbd664071ede
little-qiang <346669517@qq.com>
2016-12-20 17:08:46 +0800

    commit first chapter

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require	__DIR__.'/../vendor/autoload.php';
+
+$app = new Illuminate\Container\Container;
+
+with(new Illuminate\Events\EventServiceProvider($app))->register();
+with(new Illuminate\Routing\RoutingServiceProvider($app))->register();
+
+require __DIR__.'/../app/Http/routes.php';
+
+$request = Illuminate\Http\request::createFromGlobals();
+
+$response = $app['router']->dispatch($request);
+
+$response->send();
