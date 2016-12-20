c256cd2b27a5b2b32c9120efa21859bf356bd77d
little-qiang <346669517@qq.com>
2016-12-20 17:58:22 +0800

    update

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ with(new Illuminate\Routing\RoutingServiceProvider($app))->register();
 
 require __DIR__.'/../app/Http/routes.php';
 
-$request = Illuminate\Http\request::createFromGlobals();
+$request = Illuminate\Http\Request::createFromGlobals();
 
 $response = $app['router']->dispatch($request);
 
